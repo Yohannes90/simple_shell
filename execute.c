28f9a2e8d9d548d15963ce199232bcc864147f6b
marmad <marwane.madani99@gmail.com>
2023-07-26 07:07:34 -0700

    betty problem

--- execute.c
+++ execute.c
@@ -8,32 +8,32 @@
  */
 int new_process(char **args)
 {
-        pid_t pid;
-        int status;
+	pid_t pid;
+	int status;
 
-        pid = fork();
-        if (pid < 0)
-        {
-                perror("Error: failed to fork child process\n");
-        }
-        else if (pid == 0)
-        {
-                if (execvp(args[0], args) == -1)
-                {
-                        perror("./shell");
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                do {
-                        if (waitpid(pid, &status, WUNTRACED) == -1)
-                        {
-                                perror("error");
-                        }
-                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-        return (-1);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error: failed to fork child process\n");
+	}
+	else if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		do {
+			if (waitpid(pid, &status, WUNTRACED) == -1)
+			{
+				perror("error");
+			}
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (-1);
 }
 
 /**
